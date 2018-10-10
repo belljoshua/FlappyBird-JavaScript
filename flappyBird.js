1d4e5e84852268e1bd20ea4c665feef931c86e7d
belljoshua <belljos@oregonstate.edu>
2018-10-10 11:20:48 -0700

    Add files via upload
    Modified fall speed

--- flappyBird.js
+++ flappyBird.js
@@ -9,11 +9,11 @@ var fg = new Image();
 var pipeNorth = new Image();
 var pipeSouth = new Image();
 
-bird.src = "images/bird.png";
-bg.src = "images/bg.png";
-fg.src = "images/fg.png";
-pipeNorth.src = "images/pipeNorth.png";
-pipeSouth.src = "images/pipeSouth.png";
+bird.src = "FlappyBird-JavaScript/images/bird.png";
+bg.src = "FlappyBird-JavaScript/images/bg.png";
+fg.src = "FlappyBird-JavaScript/images/fg.png";
+pipeNorth.src = "FlappyBird-JavaScript/images/pipeNorth.png";
+pipeSouth.src = "FlappyBird-JavaScript/images/pipeSouth.png";
 
 
 // some variables
@@ -33,15 +33,15 @@ var score = 0;
 var fly = new Audio();
 var scor = new Audio();
 
-fly.src = "sounds/fly.mp3";
-scor.src = "sounds/score.mp3";
+fly.src = "FlappyBird-JavaScript/sounds/fly.mp3";
+scor.src = "FlappyBird-JavaScript/sounds/score.mp3";
 
 // on key down
 
 document.addEventListener("keydown",moveUp);
 
 function moveUp(){
-    bY -= 25;
+    bY -= 30;
     fly.play();
 }
 
